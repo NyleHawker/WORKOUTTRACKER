f177139611d15807c4b07176ba5024dd96bf21ea
unknown <em.vannin.2001@gmail.com>
2022-06-02 20:34:39 +0700

    new mod

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -47,7 +47,7 @@ class PagesController extends Controller
         // query
         $searchFood = $request->input('foodsearch');
         // when pushing to heroku, comment out the below code..
-        $foods = Food::where('food', 'LIKE', "%{$searchFood}%")->paginate(2);
+        $foods = Food::where('food', 'iLIKE', '%' . strtolower($searchFood) . '%')->paginate(2);
 
         // validate
         /*$this->validate($request, [
